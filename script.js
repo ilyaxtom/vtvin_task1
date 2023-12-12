bc6bb1d0e1e45a1b748c6bc458732c45413a8509
xtom38 <xtom38@gmail.com>
2023-12-12 15:35:41 +0300

    addaptive header

---
+++ script.js
@@ -0,0 +1,9 @@
+let show_burger = () => {
+    let burger_button = document.querySelector('.burger_menu');
+    burger_button.classList.toggle('clicked');
+}
+
+let show_mobile_burger = () => {
+    let burger_button = document.querySelector('.mobile_burger_menu');
+    burger_button.classList.toggle('clicked');
+}
